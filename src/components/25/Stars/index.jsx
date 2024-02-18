fcc6e1dc053ffef4287cb1f0836b7bb7cde71e08
briandave-dev <briandave.dev@gmail.com>
2024-02-18 02:06:45 +0100

    changes

---
+++ src/components/25/Stars/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function Stars({ noOfStars = 5 }) {
+
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
+
+    function handleClick (currentIndex) {
+        setRating(currentIndex)
+    }
+
+    function handleHover (currentIndex) {
+        setHover(currentIndex)
+    }
+
+    function handleLeave () {
+        setHover(rating)
+    }
+
+  return (
+    <div className="h-[60vh] flex items-start justify-center">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1
+
+        return (
+        <FaStar
+        key={index}
+        className={index <= (hover || rating) ? "text-yellow-300" : "text-black"}
+        onClick={() => handleClick(index)}
+        onMouseMove={() => handleHover(index)}
+        onMouseLeave={() => handleLeave()}
+        size={40}
+        />
+        )
+      })}
+    </div>
+  );
+}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,14 @@
 import Accordion from "./25/Accordion/Accordion";
 import CreateRandomValue from "./25/Random-color";
 import "../App.css"
+import Stars from "./25/Stars";
 
 const Contact = () => {
     return ( 
         <>
-            <Accordion/> 
-            <CreateRandomValue/>
+            {/* <Accordion/> 
+            <CreateRandomValue/> */}
+            <Stars noOfStars={5}/>
         </>
      );
 }
