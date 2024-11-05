6ee153febed7cc0caf4ae00179c8102866f3136f
briandave-dev <briandave.dev@gmail.com>
2024-11-05 21:01:15 +0100

    done

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Zenithe from "./test/Zenithe";
 
 const Index = () => {
-    return ( 
-        <div className="grid place-items-center h-[80vh] w-[100vw]">
-            <p className="font-bold text-lg">Welcome to Dave's simple react projects</p>
-        </div>
-     );
-}
- 
+  return (
+    <div className="grid place-items-center h-[80vh] w-[100vw]">
+      <p className="font-bold text-lg">
+        Welcome to Dave's simple react projects
+      </p>
+      <Zenithe />
+    </div>
+  );
+};
+
 export default Index;
---
+++ src/components/test/Zenithe.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const DisplayData = () => {
+  const [data, setData] = useState(null);
+  const endpoint =
+    "https://zenbio.zenitheinsurance.com:9002/orass/Zen-renouvellement/1001-1010002998";
+
+  useEffect(() => {
+    axios
+      .get(endpoint)
+      .then((response) => {
+        console.log(response.data); // Log data to console
+        setData(response.data); // Set data to state
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="p-4 bg-gray-100 text-gray-900 rounded shadow-lg">
+      <h1 className="text-xl font-bold mb-4">API Response</h1>
+      <pre className="bg-white p-4 rounded overflow-auto text-xs">
+        {data ? JSON.stringify(data, null, 2) : "Loading..."}
+      </pre>
+    </div>
+  );
+};
+
+export default DisplayData;
