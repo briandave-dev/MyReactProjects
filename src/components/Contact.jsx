ed872d889234ae15d380e37b7175bb25ffd1c8b9
briandave-dev <briandave.dev@gmail.com>
2024-11-12 22:10:18 +0100

    done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
       {/* <Requests /> */}
       {/* <Test /> */}
       {/* <Todos /> */}
-      <Calculator />
+      {/* <Calculator /> */}
       {/* <Auth /> */}
     </>
   );
